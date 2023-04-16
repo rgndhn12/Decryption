11327f571acadddc58485cea75a493d68936b188
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-16 10:23:53 +0800

    Pseudocode

---
+++ Decrypt.py
@@ -0,0 +1,12 @@
+# Dahan, Regine Fae M. (BSCPE 1-5) DECRYPTION
+
+# ask the user for input
+
+# user's input to be decrypt through...
+#   if *, substitute it to a
+#   if &, substitute it to e
+#   if #, substitute it to i
+#   if +, substitute it to o
+#   if !, substitute it to u
+
+# display the output
