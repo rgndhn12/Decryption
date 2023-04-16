0980f198981a1699d71e1e38a1687f81a45deaa3
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-16 10:41:08 +0800

    * is working!

--- Decrypt.py
+++ Decrypt.py
@@ -2,12 +2,21 @@
 
 # ask the user for input
 user_input = input (' Type your string >>> ')
+the_output = ''
 
 # user's input to be decrypt through...
-#   if *, substitute it to a
+for i in range (len(user_input)):
+
+#   if *, substitute it to 
+    if user_input[i] == '*':
+        the_output += 'a'
+    else:
+        the_output += user_input[i]
+
 #   if &, substitute it to e
 #   if #, substitute it to i
 #   if +, substitute it to o
 #   if !, substitute it to u
 
 # display the output
+print ('The string\'s decryption is >>>',the_output)
