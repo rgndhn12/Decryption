8211d15d99f993f4788d065e3eff212885e9e6aa
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-16 11:02:57 +0800

    Add feature ( Try Again? : While Loop )

--- Decrypt.py
+++ Decrypt.py
@@ -1,52 +1,70 @@
 # Dahan, Regine Fae M. (BSCPE 1-5) DECRYPTION
 
-#introduction
-import pyfiglet
-import time
+def main():
 
-the_intro ='Starting...'
-print(pyfiglet.figlet_format(the_intro,font="digital"))
-time.sleep(3)
-print('Hello! This program is entitled DECRYPTION! It will accept a\nstring as encrypted text, then the program will decrypt it.')
-time.sleep(2)
+    #introduction
+    import pyfiglet
+    import time
 
-now_begin ='Let\'s start'
-print(pyfiglet.figlet_format(now_begin,font="bubble"))
-time.sleep (2)
+    the_intro ='Starting...'
+    print(pyfiglet.figlet_format(the_intro,font="digital"))
+    time.sleep(3)
+    print('Hello! This program is entitled DECRYPTION! It will accept a\nstring as encrypted text, then the program will decrypt it.')
+    time.sleep(2)
 
-# ask the user for input
-user_input = input (' Type your string >>> ')
-the_output = ''
+    now_begin ='Let\'s start'
+    print(pyfiglet.figlet_format(now_begin,font="bubble"))
+    time.sleep (2)
 
-# user's input to be decrypt through...
-for i in range (len(user_input)):
+    # ask the user for input
+    user_input = input (' Type your string >>> ')
+    the_output = ''
 
-#   if *, substitute it to 
-    if user_input[i] == '*':
-        the_output += 'a'
-    
-#   if &, substitute it to e
-    elif user_input[i] == '&':
-        the_output += 'e'
+    # user's input to be decrypt through...
+    for i in range (len(user_input)):
 
-#   if #, substitute it to i
-    elif user_input[i] == '#':
-        the_output += 'i'
-
-#   if +, substitute it to o
-    elif user_input[i] == '+':
-        the_output += 'o'
+    #   if *, substitute it to a
+        if user_input[i] == '*':
+            the_output += 'a'
         
-#   if !, substitute it to u
-    elif user_input[i] == '!':
-        the_output += 'u'
-    
-    else:
-        the_output += user_input[i]
+    #   if &, substitute it to e
+        elif user_input[i] == '&':
+            the_output += 'e'
 
-# display the output
-time.sleep(1)
-the_decryption = ' Decrypting the string...'
-print(pyfiglet.figlet_format(the_decryption,font="digital"))
-time.sleep(2)
-print ('The string\'s decryption is >>>',the_output)
+    #   if #, substitute it to i
+        elif user_input[i] == '#':
+            the_output += 'i'
+
+    #   if +, substitute it to o
+        elif user_input[i] == '+':
+            the_output += 'o'
+            
+    #   if !, substitute it to u
+        elif user_input[i] == '!':
+            the_output += 'u'
+        
+        else:
+            the_output += user_input[i]
+
+    # display the output
+    time.sleep(1)
+    the_decryption = ' Decrypting the string...'
+    print(pyfiglet.figlet_format(the_decryption,font="digital"))
+    time.sleep(2)
+    print ('The string\'s decryption is >>>',the_output)
+
+    #Try Again?
+    time.sleep(2)
+    while True:
+        print('==========================================================================')
+        try_again = input (' PRESS 1 if you want to try again. PRESS 2 if otherwise. ')
+
+        if (try_again == '1'):
+            main()
+        elif (try_again == '2'):
+            print(' Thank you for using my program : ) ')
+            exit()
+        else:
+            print('Invalid.')
+
+main()
