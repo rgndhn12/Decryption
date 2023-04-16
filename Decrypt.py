d86345bf9444609d50b54095910d222d961b43dc
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-16 10:44:13 +0800

    & is working!

--- Decrypt.py
+++ Decrypt.py
@@ -10,13 +10,18 @@ for i in range (len(user_input)):
 #   if *, substitute it to 
     if user_input[i] == '*':
         the_output += 'a'
-    else:
-        the_output += user_input[i]
-
+    
 #   if &, substitute it to e
+    elif user_input[i] == '&':
+        the_output += 'e'
+
 #   if #, substitute it to i
 #   if +, substitute it to o
 #   if !, substitute it to u
-
+    
+    
+    
+    else:
+        the_output += user_input[i]
 # display the output
 print ('The string\'s decryption is >>>',the_output)
