e9355a9a7ae6d2daf823410b2fa0df88e7b55af5
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-16 10:28:08 +0800

    the user can now input a string!

--- Decrypt.py
+++ Decrypt.py
@@ -1,6 +1,7 @@
 # Dahan, Regine Fae M. (BSCPE 1-5) DECRYPTION
 
 # ask the user for input
+user_input = input (' Type your string >>> ')
 
 # user's input to be decrypt through...
 #   if *, substitute it to a
